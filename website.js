f15e948016a00a3f7bd39be192d5ef96808c0b1e
Matt Frey <matthew.t.frey@gmail.com>
2016-07-09 23:32:57 -0400

    first commit

---
+++ website.js
@@ -0,0 +1,54 @@
+var winCounter =0; 
+
+function hideCoins(){
+	document.getElementById('heads').style.display = 'none';
+	document.getElementById('tails').style.display = 'none';
+	document.getElementById('tailsButton').setAttribute("disabled", true);
+	document.getElementById('headsButton').setAttribute("disabled", true);
+	
+	
+}
+
+function isHeads() {
+	return Math.random() < .5; 
+}
+
+function flip(choice){
+	hideCoins();
+	document.getElementById('spin').style.display = '';
+
+	if(isHeads()){
+
+		if(choice == 'heads'){
+			winCounter++;
+		}else{
+			winCounter=0;
+		}	
+
+		setTimeout(function () {
+			document.getElementById('spin').style.display = 'none';
+			document.getElementById('heads').style.display = '';
+			updateScore();		
+		}, 1000)
+
+	}else{
+
+		if(choice == 'tails'){
+			winCounter++;
+		}else{
+			winCounter=0;
+		}	
+		setTimeout(function () {
+			document.getElementById('spin').style.display = 'none';
+			document.getElementById('tails').style.display = '';
+			
+			updateScore();
+		}, 1000)
+	}
+}
+
+function updateScore() {
+	document.getElementById('scoreLabel').innerHTML = winCounter.toString();
+	document.getElementById('tailsButton').removeAttribute("disabled");  
+	document.getElementById('headsButton').removeAttribute("disabled");
+}
